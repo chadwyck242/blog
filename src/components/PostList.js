721ba5ffd37cb74921ea016e9011f6376404d9e1
Chad Sisk <chadwyck.codeart@gmail.com>
2019-02-06 20:46:54 -0700

    Add componentDidMount lifecycle method to PostList component: calls the action creator fetchPosts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,6 +2,10 @@ import React from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return <div>PostList</div>;
   }
