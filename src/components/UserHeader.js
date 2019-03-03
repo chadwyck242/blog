81687cbe1fb3f887e5301e4c614189cc6fa9bfc4
chadwyck242 <chadwyck.codeart@gmail.com>
2019-03-03 11:19:01 -0700

    Add componentDidMount to UserHeader component to ensure a user is fetched

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -5,9 +5,19 @@ import { fetchUser } from "../actions";
 // UserHeader class based component, displayed in PostList component
 // Display the user information in a blog post
 class UserHeader extends React.Component {
+  // ensures that a user is fetched upon loading of component
+  componentDidMount() {
+      // call the fetchUser action creator to get a blog user
+      // function is called with the userId props from PostList
+      this.props.fetchUser(this.props.userId);
+  }
+
   render() {
     return <div>User Header</div>;
   }
 }
 
-export default connect(null, { fetchUser })(UserHeader);
+export default connect(
+  null,
+  { fetchUser }
+)(UserHeader);
