3a102dea7aa5ceaf0e8e20f031f5a6e3d7534630
chadwyck242 <chadwyck.codeart@gmail.com>
2019-03-03 10:24:37 -0700

    Create UserHeader class based component

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,5 +21,5 @@ export const fetchPosts = () => async dispatch => {
 export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
-  dispatch({ type: 'FETCH_USER', payload: response.data });
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
