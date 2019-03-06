0db170699404575754ea6eaf02f76fdd38aa2f16
chadwyck242 <chadwyck.codeart@gmail.com>
2019-03-05 19:43:16 -0700

    Create usersReducer.js file, watches for action of type FETCH_USER, returns desired state and action payload

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,8 @@
+export default (state = [], action) => {
+    switch (action.type) {
+        // in index.js we have the reducer functions defined,
+        // in this case we want the action of 'FETCH_USER'
+        case 'FETCH_USER':
+            return [...state, action.payload];
+    }
+};
