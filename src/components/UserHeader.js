4dbed9f14398903bf3536ff19da6339aabcd2b92
chadwyck242 <chadwyck.codeart@gmail.com>
2019-03-05 20:10:22 -0700

    Add find() function to UserHeader component to find a user by given id, and return to the const user

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,17 +7,25 @@ import { fetchUser } from "../actions";
 class UserHeader extends React.Component {
   // ensures that a user is fetched upon loading of component
   componentDidMount() {
-      // call the fetchUser action creator to get a blog user
-      // function is called with the userId props from PostList
-      this.props.fetchUser(this.props.userId);
+    // call the fetchUser action creator to get a blog user
+    // function is called with the userId props from PostList
+    this.props.fetchUser(this.props.userId);
   }
 
   render() {
+    // the JavaScript built-in find function used here
+    // can accept functions as well, return item is found (T / F)
+    const user = this.props.users.find(user => user.id === this.props.userId);
+
+    if (!user) {
+      return null;
+    }
+
     return <div>User Header</div>;
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return { users: state.users };
 };
 
